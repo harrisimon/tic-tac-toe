ed3c5de6aa271ec1d8b4bfc0cf4b5e26fbef59dc
harrisimon <harrisimon@gmail.com>
2022-09-10 15:33:52 -0400

    afternoon commits

--- index.js
+++ index.js
@@ -1,31 +1,66 @@
+const playerX = 'X'
+const playerO = 'O'
 
-// const playerOneMove = () => {
+const xPlayerMoves = []
+const oPlayerMoves = []
+const winningCombos = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [7,5,3],
+        [1,5,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9]
+]
+const boardMoves = []
+if (xPlayerMoves.includes(winningCombos)){
+        console.log("win")
+}
 
-// }
-// const playerTwoMove = () => {
-
-// }
-
-const board = document.querySelector("#container")
+const board = document.querySelector(".container")
+// console.log(board)
 
 const boxes = board.querySelectorAll('.box')
-
-
-
-//append content
-// const box1 = document.getElementById('box1')
-// const content = document.createTextNode('34')
-// box1.appendChild(content)
+// console.log(boxes)
 
 //clear the board
 const clearBoard = () => {
-        boxes.forEach(Element =>console.log(Element.innerHTML = '' ))       
+        boxes.forEach(Element =>console.log(Element.innerHTML = '' ))
+        
 }
-
-
 
 const resetButton = document.querySelector('.reset')
 
 resetButton.addEventListener('click', clearBoard)
 
-console.log(boxes.text)
+//---------------------------------
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+})
+
+let nextPlayer = playerO
+//adding and storing moves
+const boxClicked = (e) => {
+        const clicked = e.target
+        if (clicked.className === 'box' && clicked.innerHTML === ''){
+                nextPlayer = nextPlayer === playerO ? playerX : playerO;
+                console.log(nextPlayer)
+                e.target.innerHTML = nextPlayer
+                boardMoves.push(nextPlayer,e.target.id)
+                if (nextPlayer === playerX){
+                        xPlayerMoves.push(e.target.id)
+                } else {
+                        oPlayerMoves.push(e.target.id)
+                }                        
+        }  
+        const currPlayerDisplay = document.querySelector('.playerturn')
+        if (nextPlayer === playerX){
+                currPlayerDisplay.innerHTML = 'O Plays'
+        } else {
+                currPlayerDisplay.innerHTML = 'X Plays'
+}   
+}
+
+document.addEventListener('click', boxClicked)
