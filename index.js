8f384ce7208670f895a6b3775ab0517dd3cf7947
harrisimon <harrisimon@gmail.com>
2022-09-10 10:16:17 -0400

    orning changes

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-let board = ['','','','','','','','','']
 
-const player 
+// const playerOneMove = () => {
+
+// }
+// const playerTwoMove = () => {
+
+// }
+
+const board = document.querySelector("#container")
+// console.log(board)
+const box = board.getElementsByClassName('box')
+// console.log(box)
+
+const addX = (event) => {
+        // const box = board.getElementsByClassName('box')
+        const ex = box.innerHTML = 'X'
+}
+
+//append content
+const box1 = document.getElementById('box1')
+const content = document.createTextNode('34')
+box1.appendChild(content)
+
+//clear the board
+const clear = document.createTextNode('')
+board
+
